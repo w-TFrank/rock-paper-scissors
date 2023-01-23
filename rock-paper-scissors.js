717f31fe2dfd31b0ceb8cab1b40f41214a133472
TFrank <madmonsterHI@gmail.com>
2023-01-23 17:14:00 -0500

    added buttons for choices & text that says human & comp choice each round

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,5 +1,14 @@
 let computerSelection;
 let playerSelection;
+let round = 1;
+let winCounter = 0;
+let lossCounter = 0;
+let tieCounter= 0;
+const roundDiv = document.querySelector('#round');
+let playerSelectionHeading = document.createElement('h4');
+let computerSelectionHeading = document.createElement('h4');
+roundDiv.appendChild(playerSelectionHeading);
+roundDiv.appendChild(computerSelectionHeading);
 
 function getComputerChoice() {
     
@@ -8,53 +17,46 @@ function getComputerChoice() {
     computerSelection = Math.floor(Math.random() * (max - min) + min);
 
     if (computerSelection === 1) {
-        return computerSelection = "rock";
+        return "rock";
     }
     else if (computerSelection === 2) {
-        return computerSelection = "paper";
+        return "paper";
     }
     else {
-        return computerSelection = "scissors";
-    }
-}
-
-function getPlayerChoice() {
-    playerSelection = prompt("Rock, paper, or scissors?");
-    playerSelection = playerSelection.toLowerCase();
-
-    if ((playerSelection === "rock") || (playerSelection === "paper") || 
-        (playerSelection === "scissors")) {
-        return playerSelection;
-    }
-    else {
-        alert("Please enter rock, paper, or scissors.");
-        return getPlayerChoice();
+        return "scissors";
     }
 }
 
 function playRound(playerSelection, computerSelection) {
-    console.log("You chose " + playerSelection);
-    console.log("The computer chose " + computerSelection);
+    playerSelectionHeading.textContent = ("You chose " + playerSelection + ".");
+    computerSelectionHeading.textContent = ("The computer chose " + computerSelection + ".");
 
     if (playerSelection === computerSelection) {
+        tieCounter++;
         return "tie";
     }
     else if ((playerSelection === "rock") && (computerSelection === "scissors")) {
+        winCounter++;
         return "win";
     }
     else if ((playerSelection === "rock") && (computerSelection === "paper")) {
+        lossCounter++;
         return "loss";
     }
     else if ((playerSelection === "scissors") && (computerSelection === "rock")) {
+        lossCounter++;
         return "loss";    
     }
     else if ((playerSelection === "scissors") && (computerSelection === "paper")) {
+        winCounter++;
         return "win";
     }
     else if ((playerSelection === "paper") && (computerSelection === "rock")) {
+        winCounter++;
         return "win";
     }
     else if ((playerSelection === "paper") && (computerSelection === "scissors")) {
+        lossCounter++;
         return "loss";
     }
     else {
@@ -63,46 +65,34 @@ function playRound(playerSelection, computerSelection) {
 }
 
 function game() {
-    let winCounter = 0;
-    let lossCounter = 0;
-    let tieCounter= 0;
-    let round;
-    for (let i = 0; i < 5; i++) {
-        round = playRound(getPlayerChoice(), getComputerChoice());
-        if (round === "win") {
-            console.log("You chose " + playerSelection + " and the computer chose " +
-                        computerSelection + ". You win this round!");
-            winCounter++;
-            if (winCounter === 3) {
-                break;
-            }
-        }
-        else if (round === "loss") {
-            console.log("You chose " + playerSelection + " and the computer chose " +
-                        computerSelection + ". You lose this round!");
-            lossCounter++;
-            if (lossCounter === 3) {
-                break;
-            }
-        }
-        else {
-            console.log("You chose " + playerSelection + " and the computer chose " +
-                        computerSelection + ". You tied this round!");
-            tieCounter++;
-        }
-    }
     console.log("rounds won: " + winCounter);
     console.log("rounds lost: " + lossCounter);
     console.log("rounds tied: " + tieCounter);
-    if (winCounter > lossCounter) {
+    
+    if ((round === 5) && (winCounter > lossCounter)) {
         console.log("Congratulations! You won the game!");
     }
-    else if (winCounter < lossCounter) {
+    else if ((round === 5) && (winCounter < lossCounter)) {
         console.log("Oh no! You lost the game!");
     }
-    else {
+    else if ((round === 5) && (winCounter === lossCounter)){
         console.log("You tied the game!");
+    }
+
+    if (round === 5) {
+        round = 0;
+        winCounter = 0;
+        lossCounter = 0;
+        tieCounter = 0;
     }
 }
 
-game();
+let buttons = document.querySelectorAll('button');
+    
+buttons.forEach(function (i) {
+    i.addEventListener('click', function () {
+        playRound(i.id, getComputerChoice());
+        game();
+        round++;
+    });
+});
